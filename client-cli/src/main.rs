1010f20476e25fadb84a812431dda12afaf04e3e
Scott Lamb <slamb@slamb.org>
2021-04-22 20:26:01 -0700

    "client signals": display signal and state names

--- client-cli/src/main.rs
+++ client-cli/src/main.rs
@@ -1,6 +1,6 @@
-use std::collections::{HashSet, hash_map::RandomState};
+use std::collections::{HashMap, HashSet, hash_map::RandomState};
 
-use failure::Error;
+use failure::{Error, format_err};
 use std::iter::FromIterator;
 use structopt::StructOpt;
 
@@ -36,21 +36,49 @@ async fn main() -> Result<(), Error> {
     let cli = client::Client::new(opt.nvr, opt.cookie);
     match opt.cmd {
         Command::Signals { signals, start, end } => {
-            let resp = cli.signals(&client::SignalsRequest {
+            let toplevel_resp = cli.top_level(&client::TopLevelRequest {
+                days: false,
+                camera_configs: false,
+            }).await?;
+            let mut signal_types = HashMap::new();
+            for t in &toplevel_resp.signal_types {
+                let mut state_names = Vec::new();
+                for s in &t.states {
+                    let v = usize::from(s.value);
+                    if v >= state_names.len() {
+                        state_names.resize(v + 1, None);
+                    }
+                    state_names[v] = Some(s.name.as_str());
+                }
+                signal_types.insert(t.uuid, state_names);
+            }
+            let mut signals_by_id = HashMap::new();
+            for s in &toplevel_resp.signals {
+                signals_by_id.insert(s.id, s);
+            }
+            let signals_resp = cli.signals(&client::SignalsRequest {
                 start,
                 end,
             }).await?;
             let signals = signals.map(|v| HashSet::<u32, RandomState>::from_iter(v.into_iter()));
-            for i in 0..resp.signal_ids.len() {
-                let signal = resp.signal_ids[i];
-                let state = resp.states[i];
-                let time_90k = resp.times_90k[i];
+            for i in 0..signals_resp.signal_ids.len() {
+                let signal_id = signals_resp.signal_ids[i];
+                let state_val = usize::from(signals_resp.states[i]);
+                let time_90k = signals_resp.times_90k[i];
                 if let Some(s) = signals.as_ref() {
-                    if !s.contains(&signal) {
+                    if !s.contains(&signal_id) {
                         continue;
                     }
                 }
-                println!("{}: {}: {}", time_90k, signal, state);
+                let signal = signals_by_id.get(&signal_id).ok_or_else(|| format_err!("unknown signal {}", signal_id))?;
+                let state_names = signal_types.get(&signal.type_).ok_or_else(|| format_err!("signal {} references unknown type {}", &signal.short_name, signal.type_))?;
+                let state_name = match state_val {
+                    0 => Some("unknown"),
+                    i if i < state_names.len() => state_names[i],
+                    _ => None,
+                };
+                let state_name = state_name.ok_or_else(|| format_err!("signal {} state {} unknown", &signal.short_name, state_val))?;
+                println!("{}: {}: {}", time_90k, &signal.short_name, state_name);
            }
         },
     }
