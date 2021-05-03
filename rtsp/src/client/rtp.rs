ecbb1318eb6d50f7362bae83bac70ae0b879f056
Scott Lamb <slamb@slamb.org>
2021-05-03 16:51:14 -0700

    be more permissive on stream open

--- rtsp/src/client/rtp.rs
+++ rtsp/src/client/rtp.rs
@@ -21,13 +21,22 @@ pub trait PacketHandler {
     fn end(&mut self) -> Result<(), Error>;
 }
 
-/// Ensures packets have the correct SSRC, are in sequence with no gaps, and have reasonable timestamps.
+/// Maximum number of skipped initial sequence numbers.
+/// At least with a [Dahua
+/// IPC-HDW5442T-ZE](https://www.dahuasecurity.com/products/All-Products/Network-Cameras/WizMind-Series/5-Series/4MP/IPC-HDW5442T-ZE)
+/// running `V2.800.15OG004.0.T, Build Date: 2020-11-23`, the first packet's sequence number is sometimes higher than the
+/// that specified in the `PLAY` response's `RTP-Info: ...;seq=...` field. Perhaps this happens if the next IDR frame happens just
+/// as the `PLAY` command is finishing.
+const MAX_INITIAL_SEQ_SKIP: u16 = 128;
+
+/// Ensures packets have the correct SSRC, are in sequence with (almost) no gaps, and have reasonable timestamps.
+///
+/// Exception: it allows a gap in the sequence at the beginning, as explained at [`MAX_INITIAL_SEQ_SKIP`].
 ///
 /// This is the simplest and easiest-to-debug policy. It may suffice for
-/// connecting to an IP camera via RTP/AVP/TCP. We'll have to see if any IP
-/// cameras have a race between the `seq` returned in the `RTP-Info` header
-/// and sending the first RTP packet, if they skip sequence numbers when the
-/// TCP window fills, etc.
+/// connecting to an IP camera via RTP/AVP/TCP. We'll have to see if cameras
+/// skip sequence numbers in any other cases, such as when the TCP window fills
+/// and/or the camera is overloaded.
 ///
 /// It definitely wouldn't work well when using UDP or when using a proxy which
 /// may be using UDP for the backend:
@@ -46,6 +55,7 @@ pub struct StrictSequenceChecker<'a> {
     ssrc: u32,
     next_seq: u16,
     inner: &'a mut dyn PacketHandler,
+    max_seq_skip: u16,
 }
 
 impl<'a> StrictSequenceChecker<'a> {
@@ -54,6 +64,7 @@ impl<'a> StrictSequenceChecker<'a> {
             ssrc,
             next_seq,
             inner,
+            max_seq_skip: MAX_INITIAL_SEQ_SKIP,
         }
     }
 }
@@ -64,16 +75,19 @@ impl<'a> super::ChannelHandler for StrictSequenceChecker<'a> {
             Err(e) => bail!("corrupt RTP packet while expecting seq={:04x} at {:#?}: {}", self.next_seq, &rtsp_ctx, e),
             Ok(p) => p,
         };
-        if pkt.header.ssrc != self.ssrc || pkt.header.sequence_number != self.next_seq {
-            bail!("Expected ssrc={:08x} seq={:04x} got ssrc={:08x} seq={:04x} at {:#?}", self.ssrc, self.next_seq, pkt.header.ssrc, pkt.header.sequence_number, &rtsp_ctx);
-        }
         let timestamp = match timeline.advance(pkt.header.timestamp) {
             Ok(ts) => ts,
             Err(e) => return Err(e.context(format!("timestamp error in seq={:04x} {:#?}", pkt.header.sequence_number, &rtsp_ctx)).into()),
         };
+        if pkt.header.ssrc != self.ssrc
+           || pkt.header.sequence_number.wrapping_sub(self.next_seq) > self.max_seq_skip {
+            bail!("Expected ssrc={:08x} seq={:04x} got ssrc={:08x} seq={:04x} ts={} at {:#?}",
+                  self.ssrc, self.next_seq, pkt.header.ssrc, pkt.header.sequence_number, timestamp, &rtsp_ctx);
+        }
         //println!("pkt{} seq={:04x} ts={}", if pkt.header.marker { "   " } else { "(M)"}, self.next_seq, &timestamp);
         //println!("{:?}", data.hex_dump());
-        self.next_seq = self.next_seq.wrapping_add(1);
+        self.next_seq = pkt.header.sequence_number.wrapping_add(1);
+        self.max_seq_skip = 0;
         self.inner.pkt(Packet {
             rtsp_ctx,
             timestamp,
